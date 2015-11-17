f33827eb82a471801e62813a7394818e333d4053
Artem Aminov <artemaminov@gmail.com>
2015-11-17 18:55:55 +0500

    add admin preview

---
+++ app/assets/javascripts/admin/cloudzoom_thumbnails.js.coffee
@@ -0,0 +1,2 @@
+$ ->
+  CloudZoom.quickStart()
--- app/overrides/cloudzoom.rb
+++ app/overrides/cloudzoom.rb
@@ -1,16 +1,23 @@
 Deface::Override.new(
-                    :virtual_path => "spree/products/_thumbnails",
-                    :name => "add_additional_attr_to_thumbnails",
-                    :set_attributes => "ul#product-thumbnails li",
+                    :virtual_path => 'spree/products/_thumbnails',
+                    :name => 'add_additional_attr_to_thumbnails',
+                    :set_attributes => 'ul#product-thumbnails li',
                     :attributes => {
                         :id => "<%= i.id %>",
-                        :"data-variant-id" => "<%= i.viewable.id %>"
+                        :'data-variant-id' => "<%= i.viewable.id %>"
                     }
 )
 
 Deface::Override.new(
-                    :virtual_path => "spree/products/_thumbnails",
-                    :name => "add_link_to_large",
-                    :replace_contents => "ul#product-thumbnails li",
-                    :partial => "spree/products/cloudzoom"
+                    :virtual_path => 'spree/products/_thumbnails',
+                    :name => 'add_link_to_large',
+                    :replace_contents => 'ul#product-thumbnails li',
+                    :partial => 'spree/products/cloudzoom'
+)
+
+Deface::Override.new(
+                    :virtual_path => 'spree/admin/products/index',
+                    :name => 'add_additional_attr_to_thumbnails',
+                    :replace_contents => '[data-hook="admin_products_index_rows"] td:nth-child(2)',
+                    :partial => 'spree/admin/products/cloudzoom',
 )
